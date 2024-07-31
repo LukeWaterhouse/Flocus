651a6a6647b7fe1c9820532c1a021eeb4995d0f3
luke.waterhouse <luke.waterhouse@ihsmarkit.com>
2024-07-31 16:43:48 +0100

    feat: add tests for email validation hadnler

---
+++ Flocus.Identity.Tests/Services/RegistrationServicesTests/RegistrationValidationServicesTests/HandlersTests/EmailValidationHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Flocus.Domain.Models.Errors;
+using Flocus.Identity.Models;
+using Flocus.Identity.Services.RegistrationServices.RegistrationValidationServices.Handlers;
+using FluentAssertions;
+using Xunit;
+
+namespace Flocus.Identity.Tests.Services.RegistrationServicesTests.RegistrationValidationServicesTests.HandlersTests;
+
+public class EmailValidationHandlerTests
+{
+    private readonly EmailValidationHandler _emailValidationHandler;
+
+    public EmailValidationHandlerTests()
+    {
+        _emailValidationHandler = new EmailValidationHandler();
+    }
+
+    [Fact]
+    public void Validate_ValidEmail_AddsNoErrors()
+    {
+        // Arrange
+        var email = "luke@hotmail.co.uk";
+
+        var errors = new List<Error>();
+        var registrationModel = new RegistrationModel(
+            "username",
+            "password",
+            email,
+            true,
+            "123");
+
+        // Act
+        _emailValidationHandler.Validate(errors, registrationModel);
+
+        // Assert
+        errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Validate_InvalidEmail_AddsError()
+    {
+        // Arrange
+        var email = "invalid email";
+
+        var errors = new List<Error>();
+        var registrationModel = new RegistrationModel(
+            "username",
+            "password",
+            email,
+            true,
+            "123");
+
+        // Act
+        _emailValidationHandler.Validate(errors, registrationModel);
+
+        // Assert
+        var expectedErrors = new List<Error>()
+        {
+            new Error(400, $"Email is not a valid format: {email}")
+
+        };
+
+        errors.Should().BeEquivalentTo(expectedErrors);
+    }
+}
