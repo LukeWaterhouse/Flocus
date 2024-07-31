8da52c1eedfa8ab84e9989767e1144deec96d014
luke.waterhouse <luke.waterhouse@ihsmarkit.com>
2024-07-31 16:10:58 +0100

    admin key service tests complete

---
+++ Flocus.Identity.Tests/Services/AdminKeyServicesTests/AdminKeyServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using Flocus.Domain.Models;
+using Flocus.Identity.Models;
+using Flocus.Identity.Services.AdminKeyServices;
+using FluentAssertions.Execution;
+using FluentAssertions;
+using System.Security.Authentication;
+using System.Security.Claims;
+using Xunit;
+
+namespace Flocus.Identity.Tests.Services.AdminKeyServicesTests;
+
+public sealed class AdminKeyServiceTests
+{
+    private readonly IdentitySettings _identitySettings;
+    private readonly AdminKeyService _adminKeyService;
+
+    private readonly string CorrectAdminKey = "adminKey";
+
+    public AdminKeyServiceTests()
+    {
+        _identitySettings = new IdentitySettings("signingKey", "issuer", "audience", CorrectAdminKey);
+        _adminKeyService = new AdminKeyService(_identitySettings);
+    }
+
+    [Fact]
+    public void CheckAdminKeyCorrect_CorrectAdminKey_DoesNotThrowException()
+    {
+        // Arrange
+        var inputAdminKey = "adminKey";
+
+        // Act
+        _adminKeyService.CheckAdminKeyCorrect(inputAdminKey);
+    }
+
+    [Fact]
+    public void CheckAdminKeyCorrect_IncorrectAdminKey_ThrowsException()
+    {
+        // Arrange
+        var inputAdminKey = "incorrect adminKey";
+
+        // Act
+        Exception exception = Record.Exception(() =>
+        {
+            _adminKeyService.CheckAdminKeyCorrect(inputAdminKey);
+        });
+
+        // Assert
+        using (new AssertionScope())
+        {
+            exception.Should().BeOfType<AuthenticationException>();
+            exception.Message.Should().Be("Admin key is not correct");
+        }
+    }
+}
