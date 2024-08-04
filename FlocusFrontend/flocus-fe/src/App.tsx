715ab03a61f8101868e01eff1f045beae127d2c6
luke.waterhouse <luke.waterhouse@ihsmarkit.com>
2024-08-04 21:36:33 +0100

    converted

--- FlocusFrontend/flocus-fe/src/App.tsx
+++ FlocusFrontend/flocus-fe/src/App.tsx
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
+
 function App() {
+
   const { unityProvider } = useUnityContext({
     loaderUrl: "WebGL/Build/WebGL.loader.js",
     dataUrl: "WebGL/Build/WebGL.data",
     frameworkUrl: "WebGL/Build/WebGL.framework.js",
-    codeUrl: "WebGL/Build/WebGL.wasm"
-  });
+    codeUrl: "WebGL/Build/WebGL.wasm"});
 
-  const [showCover, setShowCover] = useState(true);
+    const [showCover, setShowCover] = useState(true);
 
-  useEffect(() => {
-    // Remove the cover after 4 seconds
-    const timeout = setTimeout(() => {
-      setShowCover(false);
-    }, 4000);
-
-    return () => clearTimeout(timeout);
-  }, []);
+    useEffect(() => {
+      // Remove the cover after 4 seconds
+      const timeout = setTimeout(() => {
+        setShowCover(false);
+      }, 4000);
+  
+      return () => clearTimeout(timeout);
+    }, []);
 
   return (
     <div style={{ position: "relative", width: 800, height: 600 }}>
