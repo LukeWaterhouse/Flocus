2654c5a6028753e5374762f83819804f1a5f9c0b
luke.waterhouse <luke.waterhouse@ihsmarkit.com>
2024-11-12 17:20:01 +0000

    reduce coverage

--- FlocusBackend/Flocus.Domain.Tests/Services/UserServicesTests/UserServiceTests.cs
+++ FlocusBackend/Flocus.Domain.Tests/Services/UserServicesTests/UserServiceTests.cs
@@ -19,29 +19,29 @@ public sealed class UserServiceTests
         _userService = new UserService(_userRepositoryServiceMock);
     }
 
-    [Fact]
-    public async Task GetUserAsync_ValidUsername_ReturnsUser()
-    {
-        // Arrange
-        var username = "lukosparta123";
-        var clientId = Guid.NewGuid().ToString();
-        var email = "lukosparta@hotmail.com";
-        var createdAt = DateTime.UtcNow;
-        var isAdmin = true;
-        var passwordHash = "19lk234l534l5tk";
+    //[Fact]
+    //public async Task GetUserAsync_ValidUsername_ReturnsUser()
+    //{
+    //    // Arrange
+    //    var username = "lukosparta123";
+    //    var clientId = Guid.NewGuid().ToString();
+    //    var email = "lukosparta@hotmail.com";
+    //    var createdAt = DateTime.UtcNow;
+    //    var isAdmin = true;
+    //    var passwordHash = "19lk234l534l5tk";
 
-        var user = new User(clientId, email, createdAt, username, isAdmin, passwordHash);
-        _userRepositoryServiceMock.GetUserAsync(username).Returns(user);
+    //    var user = new User(clientId, email, createdAt, username, isAdmin, passwordHash);
+    //    _userRepositoryServiceMock.GetUserAsync(username).Returns(user);
 
-        // Act
-        var result = await _userService.GetUserAsync(username);
+    //    // Act
+    //    var result = await _userService.GetUserAsync(username);
 
-        // Assert
-        var expectedUser = new User(clientId, email, createdAt, username, isAdmin, passwordHash);
+    //    // Assert
+    //    var expectedUser = new User(clientId, email, createdAt, username, isAdmin, passwordHash);
 
-        using (new AssertionScope())
-        {
-            result.Should().BeEquivalentTo(expectedUser);
-        }
-    }
+    //    using (new AssertionScope())
+    //    {
+    //        result.Should().BeEquivalentTo(expectedUser);
+    //    }
+    //}
 }
